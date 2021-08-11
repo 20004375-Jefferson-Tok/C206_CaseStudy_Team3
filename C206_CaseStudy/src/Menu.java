101d9d219e4b87a3f2c65aff01f7ce2afa97d470
20004375 <20004375@LAPTOP-IDMN6CAS>
2021-08-11 16:24:31 +0800

    Added Menu sub class

--- C206_CaseStudy/src/Menu.java
+++ C206_CaseStudy/src/Menu.java
@@ -1,12 +1,3 @@
-/**
- * I declare that this code was written by me.
- * I will not copy or allow others to copy my code.
- * I understand that copying code is considered as plagiarism.
- *
- * 20004375, Aug 10, 2021 9:28:11 PM
- */
-
-package ese;
 
 public class Menu {
 	private int id;
