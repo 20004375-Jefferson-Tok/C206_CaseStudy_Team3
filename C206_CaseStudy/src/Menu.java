c2ed5b8c11f18dda5a0b4dc4494317d8e8f393eb
20004375-Jefferson-Tok <87613622+20004375-Jefferson-Tok@users.noreply.github.com>
2021-08-11 16:21:59 +0800

    Add files via upload

---
+++ C206_CaseStudy/src/Menu.java
@@ -0,0 +1,38 @@
+/**
+ * I declare that this code was written by me.
+ * I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism.
+ *
+ * 20004375, Aug 10, 2021 9:28:11 PM
+ */
+
+package ese;
+
+public class Menu {
+	private int id;
+	private String item;
+	
+	public Menu(int id, String item) {
+		this.id = id;
+		this.item = item;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getItem() {
+		return item;
+	}
+
+	public void setItem(String item) {
+		this.item = item;
+	}
+
+
+
+}
